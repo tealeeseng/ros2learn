4a7cf3a603a728d72aca0e6cb79a49f88b3dfa0e
tealeeseng <tealeeseng@gmail.com>
2020-03-28 14:13:50 +0800

    move default to script. As defaults.py cannot be checked in as per git submodule.

--- experiments/examples/MARA/train_ppo2_mlp_random.py
+++ experiments/examples/MARA/train_ppo2_mlp_random.py
@@ -57,7 +57,38 @@ def make_env():
 
 # Get dictionary from baselines/ppo2/defaults
 env_type = 'mara_mlp_random'
-alg_kwargs = get_learn_function_defaults('ppo2', env_type)
+# alg_kwargs = get_learn_function_defaults('ppo2', env_type)
+alg_kwargs = dict(
+        num_layers = 2,
+        num_hidden = 16,
+        layer_norm = False,
+        nsteps = 1024,
+        nminibatches = 4, #batchsize = nevn * nsteps // nminibatches
+        lam = 0.95,
+        gamma = 0.99,
+        noptepochs = 10,
+        log_interval = 1,
+        ent_coef = 0.0,
+        lr = lambda f: 3e-3 * f,
+        cliprange = 0.25,
+        vf_coef = 1,
+        max_grad_norm = 0.5,
+        seed = 0,
+        value_network = 'copy',
+        network = 'mlp',
+        total_timesteps = 1e8,
+        save_interval = 10,
+        # env_name = 'MARA-v0',
+        env_name = 'MARARandomTarget-v0',
+        #env_name = 'MARAReal-v0',
+        #env_name = 'MARAOrient-v0',
+        # env_name = 'MARACollision-v0',
+        # env_name = 'MARACollisionOrient-v0',
+        transfer_path = None,
+        # transfer_path = '/tmp/ros2learn/MARA-v0/ppo2_mlp/2019-02-19_12h47min/checkpoints/best',
+        trained_path = '/tmp/ros2learn/MARA-v0/ppo2_mlp/2019-04-02_13h18min/checkpoints/best'
+    )
+
 
 # Create needed folders
 timedate = datetime.now().strftime('%Y-%m-%d_%Hh%Mmin')
