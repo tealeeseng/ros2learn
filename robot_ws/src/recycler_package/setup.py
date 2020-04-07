2fa39aaf66893bcfd73838ceabc9a38c31bc4015
tealeeseng <tealeeseng@gmail.com>
2020-04-07 22:16:52 +0800

    proper joints_xyz.csv build

--- robot_ws/src/recycler_package/setup.py
+++ robot_ws/src/recycler_package/setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name+'/resource/', ['resource/joints_xyz.csv']),
+        
     ],
     install_requires=['setuptools'],
     zip_safe=True,
