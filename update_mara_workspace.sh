56d027092a2db3c9f66aa355ba36871728692ee5
tealeeseng <tealeeseng@gmail.com>
2020-04-12 13:35:39 +0800

    update camera setup on robot

--- update_mara_workspace.sh
+++ update_mara_workspace.sh
@@ -1,1 +1,3 @@
-rsync -aP mara/* ~/ros2_mara_ws/src/
+# rsync -aP mara/* ~/ros2_mara_ws/src/
+cp ./mara/mara_description/urdf/reinforcement_learning/mara_robot_gripper_140_camera_train.urdf ../ros2_mara_ws/install/share/mara_description/urdf/reinforcement_learning/mara_robot_gripper_140_camera_train.urdf
+
